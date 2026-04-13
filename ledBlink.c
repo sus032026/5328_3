80a2838aeeb2cd142445ce2b06b5c4856432bad2
sus032026 <emhr32@proton.me>
2026-04-13 15:01:33 +0300

    Change delay in file ledBlink.c

--- ledBlink.c
+++ ledBlink.c
@@ -6,7 +6,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
-  delay(1000);                      // wait for a second
+  delay(3000);                      // wait for a 3 second
   digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
-  delay(1000);                      // wait for a second
+  delay(3000);                      // wait for a 3 second
 }
